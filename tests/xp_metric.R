afff299738bdc8e40ab00f5acc9dace15c42aa69
quantitative-ecologist <49170767+quantitative-ecologist@users.noreply.github.com>
2021-06-30 14:18:25 -0400

    update packages

--- tests/xp_metric.R
+++ tests/xp_metric.R
@@ -9,16 +9,6 @@ data <- fread("./data/merged-data.csv")
 
 
 
-
-
-# Vector for cumsum calculation must start at 0 (first match)
-# -------------------------------------------------------------
-for.cumsum <- data[, .SD[which.min(timestamp)], by = mirrors_id]
-for.cumsum[, for_cumsum := 0]
-data <- merge(for.cumsum[,c(2:3,59)], data, all.y=T, by =c("timestamp", "match_id"))
-for (col in "for_cumsum") data[is.na(get(col)), (col) := 1] # replace all NAs with 1
-
-
 # solution 2 (better as I dont copy and merge)
 # ------------------------------------------------------------
 # Order timestamps
@@ -27,12 +17,6 @@ setorder(data, mirrors_id, timestamp)
 data[, first_login := ave(timestamp, mirrors_id, FUN = min)]
 data[, for_cumsum := timestamp %in% first_login]
 data[, for_cumsum := ifelse(data$for_cumsum == TRUE, 0, 1)]
-
-View(data[,.(mirrors_id, timestamp, first_login, for_cumsum)])
-View(data[,.(mirrors_id, timestamp, first_login, for_cumsum, xp_total)])
-
-
-
 
 
 
@@ -67,8 +51,5 @@ data1[, xp.focal.map := cumsum(for_cumsum), by = .(mirrors_id, map_name)]
 
 # data1[, priorxp.other.role := xp.total - xp.focal.role]
 
-# Prior experience in other maps in the focal role, variables no.3 and no.4 for Julien
-# data1[, priorxp.other.maps := xp.focal.role - xp.focal.rolemap]
-# data1[, priorxp.other.realms := xp.focal.role - xp.focal.rolerealm]
 
 # -----------------------------------------------
