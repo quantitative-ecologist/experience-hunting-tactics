6cb7784d4971a956347f955e37b4ddcebde86d6b
quantitative-ecologist <49170767+quantitative-ecologist@users.noreply.github.com>
2021-07-16 16:24:22 -0400

    update analysis plan

--- plan/compile-plan-file.jl
+++ plan/compile-plan-file.jl
@@ -1,8 +1,9 @@
 # Import package
 using Weave
 
+
 # Weave te document 
-weave("analysis-plan.Jmd",
+weave("plan\\analysis-plan.Jmd",
         doctype = "pandoc2pdf",
         fig_path = "figures",
         pandoc_options = ["--toc"]) # toc includes a table of contents
