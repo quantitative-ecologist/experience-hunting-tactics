2bb2f46a6e60eba12ac5d637e95b23ce1ad8e71f
quantitative-ecologist <49170767+quantitative-ecologist@users.noreply.github.com>
2021-07-16 14:51:10 -0400

    include weave file

---
+++ code/compile-weave.jl
@@ -0,0 +1,28 @@
+# Import package
+using Weave
+
+# To weave all the Jmd documents
+#jmd_files = filter(endswith("Jmd"), readdir())
+#weave.(jmd_files)
+
+directory = pwd()
+
+# Weave each document seperately
+weave("code/data-exploration.Jmd", 
+        fig_path = "data-exploration-figs",
+        doctype = "pandoc2pdf",
+        fig_ext = ".png")
+
+weave("analysis-plan.Jmd",
+        doctype = "pandoc2pdf",
+        fig_path = "figures",
+        pandoc_options = ["--toc"])
+
+#weave("code/experience-metrics.Jmd",
+#        fig_path = "experience-metrics-figs",
+#        doctype = "pandoc2pdf",
+#        fig_ext = ".png")
+
+#weave("code/figure1.Jmd", 
+#        fig_path = "/figures/figure1",
+#        fig_ext = ".png")
