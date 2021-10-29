83f9392e4540bbbb11ee42265ddf230c39aae9de
quantitative-ecologist <49170767+quantitative-ecologist@users.noreply.github.com>
2021-10-29 15:48:46 -0400

    moved some files, create dhmlm file 2

--- code/01_model-diagnostics.R
+++ code/01_model-diagnostics.R
@@ -54,8 +54,52 @@ print(object.size(model), units = "MB")
 # 2. Basic model diagnostics
 # =======================================================================
 
+
 # Trace plots and parameter distributions -------------------------------
+
 plot(model)
+
+
+
+# Posterior predictive checks -------------------------------------------
+
+# Check distributions
+a <- pp_check(model, resp = "Zspeednovice")
+b <- pp_check(model, resp = "Zspeedinterm")
+c <- pp_check(model, resp = "Zspeedadvanced")
+ 
+d <- pp_check(model, resp = "Zambushnovice")
+e <- pp_check(model, resp = "Zambushinterm")
+f <- pp_check(model, resp = "Zambushadvanced")
+ 
+g <- pp_check(model, resp = "Zlatencynovice")
+h <- pp_check(model, resp = "Zlatencyinterm")
+i <- pp_check(model, resp = "Zlatencyadvanced")
+ 
+# Effects plot (mean variance plot)
+j <- pp_check(model, resp = "Zspeednovice", type = "stat_2d")
+k <- pp_check(model, resp = "Zspeedinterm", type = "stat_2d")
+l <- pp_check(model, resp = "Zspeedadvanced", type = "stat_2d")
+ 
+m <- pp_check(model, resp = "Zambushnovice", type = "stat_2d")
+n <- pp_check(model, resp = "Zambushinterm", type = "stat_2d")
+o <- pp_check(model, resp = "Zambushadvanced", type = "stat_2d")
+ 
+p <- pp_check(model, resp = "Zlatencynovice", type = "stat_2d")
+q <- pp_check(model, resp = "Zlatencyinterm", type = "stat_2d")
+r <- pp_check(model, resp = "Zlatencyadvanced", type = "stat_2d")
+
+
+
+# Add criterions to the model -------------------------------------------
+
+model <- add_criterion(model, 
+                       "loo", 
+                       resp = c("Zspeednovice", "Zspeedinterm", "Zspeedadvanced",
+                                "Zambushnovice", "Zambushinterm", "Zambushadvanced",
+                                "Zlatencynovice", "Zlatencyinterm","Zlatencyadvanced",
+                                "Zpreyspeednovice", "Zpreyspeedinterm", "Zpreyspeedadvanced")
+                        )
 
 # =======================================================================
 # =======================================================================
