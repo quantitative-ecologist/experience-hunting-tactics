c571858680f8039a9333ab6295b7052fb9347fb5
quantitative-ecologist <49170767+quantitative-ecologist@users.noreply.github.com>
2022-06-22 14:11:34 -0400

    update 02A1 R and sh and 02A2.sh

--- slurm/02A2_GAMM.sh
+++ slurm/02A2_GAMM.sh
@@ -3,7 +3,7 @@
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=40
 #SBATCH --mem=32000M
-#SBATCH --time=2-16:00
+#SBATCH --time=1-10:00
 #SBATCH --mail-user=fraser_franco.maxime@courrier.uqam.ca
 #SBATCH --mail-type=ALL
 
