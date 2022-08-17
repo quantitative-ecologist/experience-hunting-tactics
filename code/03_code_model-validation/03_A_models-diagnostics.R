7c416d84a23c82b819ea7469384c60ad3b067a02
quantitative-ecologist <49170767+quantitative-ecologist@users.noreply.github.com>
2022-08-17 11:07:37 -0400

    update diagnostics file for GAMM + GLMM

--- code/03_code_model-validation/03_A_models-diagnostics.R
+++ code/03_code_model-validation/03_A_models-diagnostics.R
@@ -27,6 +27,7 @@ library(ggpubr)
 # Load models
 mod1 <- readRDS("./outputs/02_outputs_models/02A1_GAMM.rds")
 mod2 <- readRDS("./outputs/02_outputs_models/02A2_GAMM.rds")
+mod3 <- readRDS("./outputs/02_outputs_models/02A3_GLMM.rds")
 
 # Check object size
 print(object.size(mod1), units = "MB")
@@ -91,7 +92,7 @@ posterior_epred_beta_binomial2 <- function(prep) {
 
 plot(mod1)
 plot(mod2)
-
+plot(mod3)
 
 
 # Posterior predictive checks -------------------------------------------
@@ -99,21 +100,24 @@ plot(mod2)
 # Check distributions
 pp1 <- pp_check(mod1)
 pp2 <- pp_check(mod2)
- 
+pp3 <- pp_check(mod3)
  
 # Effects plot (mean variance plot)
 stat1 <- pp_check(mod1, type = "stat_2d")
 stat2 <- pp_check(mod2, type = "stat_2d")
+stat3 <- pp_check(mod3, type = "stat_2d")
 
 
 # Predicted means
 mean1 <- pp_check(mod1, type = "stat", stat = "mean")
 mean2 <- pp_check(mod2, type = "stat", stat = "mean")
+mean3 <- pp_check(mod3, type = "stat", stat = "mean")
 
 
 # Error scatter
 e_scat1 <- pp_check(mod1, type = "error_scatter_avg")
 e_scat2 <- pp_check(mod2, type = "error_scatter_avg")
+e_scat3 <- pp_check(mod3, type = "error_scatter_avg")
 
 
 
@@ -132,6 +136,12 @@ stat_fig2 <- ggarrange(pp2,
                        e_scat2,
                        ncol = 2, nrow = 2)
 
+stat_fig3 <- ggarrange(pp3,    
+                       stat3,
+                       mean3,
+                       e_scat3,
+                       ncol = 2, nrow = 2)
+
 # Export the figure
 ggexport(stat_fig1,
          filename = "./outputs/03_outputs_model-validation/03_GAMM-diagnostics1.png",
@@ -139,6 +149,10 @@ ggexport(stat_fig1,
 
 ggexport(stat_fig2,
          filename = "./outputs/03_outputs_model-validation/03_GAMM-diagnostics2.png",
+         width = 3000, height = 2500, res = 300) # more 
+
+ggexport(stat_fig3,
+         filename = "./outputs/03_outputs_model-validation/03_GLMM-diagnostics.png",
          width = 3000, height = 2500, res = 300) # more 
 
 # =======================================================================
@@ -158,26 +172,28 @@ ggexport(stat_fig2,
 # LOO-PSIS
 loo1 <- loo(mod1)
 loo2 <- loo(mod2)
+loo3 <- loo(mod3)
 
 # Save outputs
 saveRDS(loo1,
         file = "./outputs/03_outputs_model-validation/02A1_loo.rds")
 saveRDS(loo2,
         file = "./outputs/03_outputs_model-validation/02A2_loo.rds")
-
+saveRDS(loo3,
+        file = "./outputs/03_outputs_model-validation/02A3_loo.rds")
 
 
 # Compare models --------------------------------------------------------
 
 # Compare models
-loo_tab <- loo_compare(loo1, loo2)
+loo_tab <- loo_compare(loo1, loo2, loo3)
 
 # Compute table with complete information
 loo_table <- print(loo_tab, simplify = FALSE)
 
 # Save table
 saveRDS(loo_table,
-        file = "./outputs/03_outputs_model-validation/03_GAMM-lootable.rds")
+        file = "./outputs/03_outputs_model-validation/03_A_models-lootable.rds")
 
 # =======================================================================
 # =======================================================================
