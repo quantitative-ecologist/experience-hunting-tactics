dbcee85145355db8900ea8de1ee3e7bbc0af1970
quantitative-ecologist <49170767+quantitative-ecologist@users.noreply.github.com>
2022-08-17 11:00:47 -0400

    added glmm files

---
+++ code/02_code_models/02A3_GLMM.R
@@ -0,0 +1,165 @@
+# ==========================================================================
+
+#                       GLMM - random intercepts
+
+# ==========================================================================
+
+
+
+
+
+# ==========================================================================
+# 1. Prepare the data
+# ==========================================================================
+
+
+
+# Load packages ------------------------------------------------------------
+
+# Detect cores
+options(mc.cores = parallel::detectCores())
+
+# Load libraries
+library(data.table)
+library(brms)
+library(parallel)
+
+
+
+# Load data ----------------------------------------------------------------
+
+# Folder path Compute Canada
+folder <- file.path("/home", "maxime11", "projects", "def-monti", 
+                    "maxime11", "phd_project", "data")
+
+# Load data on compute canada
+data <- fread(file.path(folder, "FraserFrancoetalXXXX-data.csv"),
+              select = c("predator_id",
+                         "hunting_success",
+                         "pred_game_duration",
+                         "pred_speed",
+                         "cumul_xp_killer"))
+
+# Project path for testing
+#data <- fread("./data/FraserFrancoetalXXXX-data.csv",
+#              select = c("predator_id",
+#                         "hunting_success",
+#                         "pred_game_duration",
+#                         "pred_speed",
+#                         "cumul_xp_killer"))
+
+data <- unique(data)
+
+# Predator id as factor
+data[, predator_id := as.factor(predator_id)]
+
+
+
+# Standardise the variables (Z-scores) -------------------------------------
+
+standardize <- function (x) {(x - mean(x, na.rm = TRUE)) / 
+    sd(x, na.rm = TRUE)}
+
+data[, c("Zgame_duration",
+         "Zspeed",
+         "Zcumul_xp") := lapply(.SD, standardize), 
+     .SDcols = c(3:5)]
+
+# ==========================================================================
+# ==========================================================================
+
+
+
+
+
+# ==========================================================================
+# 2. Parametrize the model
+# ==========================================================================
+
+
+
+# Compute the custom family ------------------------------------------------
+
+beta_binomial2 <- custom_family(
+  "beta_binomial2", dpars = c("mu", "phi"),
+  links = c("logit", "log"), lb = c(NA, 0),
+  type = "int", vars = "vint1[n]"
+)
+
+
+
+# Function -----------------------------------------------------------------
+
+stan_funs <- "
+  real beta_binomial2_lpmf(int y, real mu, real phi, int T) {
+    return beta_binomial_lpmf(y | T, mu * phi, (1 - mu) * phi);
+  }
+  int beta_binomial2_rng(real mu, real phi, int T) {
+    return beta_binomial_rng(T, mu * phi, (1 - mu) * phi);
+  }
+"
+
+
+
+# Variables ----------------------------------------------------------------
+
+stanvars <- stanvar(scode = stan_funs, block = "functions")
+
+
+
+# Model formula ------------------------------------------------------------
+
+model_formula <- brmsformula(
+  hunting_success | vint(4) ~
+    Zcumul_xp +
+    Zgame_duration +
+    (1 | predator_id)
+)
+
+
+
+# Define priors ------------------------------------------------------------
+
+priors <- c(
+  # priors on fixed effects
+  set_prior("normal(0, 2)",
+            class = "b"),
+  # priors on variance parameters
+  set_prior("normal(0, 1)",
+            class = "sd"),
+  # priors on phi
+  set_prior("normal(2, 1)",
+            class = "phi")
+)
+
+# ==========================================================================
+# ==========================================================================
+
+
+
+
+
+# ==========================================================================
+# 3. Run the model
+# ==========================================================================
+
+model_glmm <- brm(formula = model_formula,
+                  family = beta_binomial2,
+                  warmup = 500, 
+                  iter = 1500,
+                  thin = 4,
+                  chains = 4,
+                  threads = threading(10),
+                  backend = "cmdstanr",
+                  inits = "0", 
+                  seed = 123,
+                  prior = priors,
+                  sample_prior = TRUE,
+                  control = list(adapt_delta = 0.99),
+                  data = data,
+                  stanvars = stanvars)
+
+saveRDS(model_glmm, file = "02A3_GLMM.rds")
+
+# ==========================================================================
+# ==========================================================================
--- code/03_code_model-validation/03_GAMM-draws.R
+++ code/03_code_model-validation/03_GAMM-draws.R
@@ -1,7 +1,7 @@
 # ==================================================================================
 
-#                        Inspect prior and posterior draws
-#                                from GAMM models
+#                       Inspect prior and posterior draws
+#                               from GAMM-GLMM models
 
 # ==================================================================================
 
@@ -26,10 +26,13 @@ library(ggplot2)
 # Import model in R session
 mod1 <- readRDS("./outputs/02_outputs_models/02A1_GAMM.rds")
 mod2 <- readRDS("./outputs/02_outputs_models/02A2_GAMM.rds")
+mod3 <- readRDS("./outputs/02_outputs_models/02A3_GLMM.rds")
+
 
 # Extract posterior draws
 posterior_fit1 <- as_draws_df(mod1)
 posterior_fit2 <- as_draws_df(mod2)
+posterior_fit3 <- as_draws_df(mod3)
 
 # ==================================================================================
 # ==================================================================================
@@ -218,6 +221,85 @@ theme_classic()
 
 # Phi parameter
 ggplot(posterior_fit2) +
+  geom_density(aes(prior_phi),
+               fill = "steelblue",
+               color = "black",
+               alpha = 0.6) +
+  geom_density(aes(phi),
+               fill = "#FC4E07",
+               color = "black",
+               alpha = 0.6) + 
+  theme_classic()
+
+# ==================================================================================
+# ==================================================================================
+
+
+
+
+
+# ==================================================================================
+# 3. Model 3
+# ==================================================================================
+
+
+
+# Plot prior and posterior draws ---------------------------------------------------
+
+# Intercept
+ggplot(posterior_fit3) +
+  geom_density(aes(prior_Intercept),
+               fill = "steelblue",
+               color = "black",
+               alpha = 0.6) +
+  geom_density(aes(Intercept),
+               fill = "#FC4E07",
+               color = "black",
+               alpha = 0.6) + 
+  theme_classic()
+
+
+# Game duration
+ggplot(posterior_fit3) +
+  geom_density(aes(prior_b),
+               fill = "steelblue",
+               color = "black",
+               alpha = 0.6) +
+  geom_density(aes(b_Zgame_duration),
+               fill = "#FC4E07",
+               color = "black",
+               alpha = 0.6) + 
+  theme_classic()
+
+
+# slope term of cumul xp
+ggplot(posterior_fit3) +
+  geom_density(aes(prior_b),
+               fill = "steelblue",
+               color = "black",
+               alpha = 0.6) +
+  geom_density(aes(b_Zcumul_xp),
+               fill = "#FC4E07",
+               color = "black",
+               alpha = 0.6) + 
+  theme_classic()
+
+
+# Standard deviation of predator ID
+ggplot(posterior_fit3) +
+  geom_density(aes(prior_sd_predator_id),
+               fill = "steelblue",
+               color = "black",
+               alpha = 0.6) +
+  geom_density(aes(sd_predator_id__Intercept),
+               fill = "#FC4E07",
+               color = "black",
+               alpha = 0.6) + 
+  theme_classic()
+
+
+# Phi parameter
+ggplot(posterior_fit3) +
   geom_density(aes(prior_phi),
                fill = "steelblue",
                color = "black",
