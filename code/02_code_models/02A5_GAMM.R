6331ec9afb72753179f95f98cebd1838b20f7bce
quantitative-ecologist <49170767+quantitative-ecologist@users.noreply.github.com>
2022-12-19 10:39:29 -0500

    update 4 to 6 GAMM R files

--- code/02_code_models/02A5_GAMM.R
+++ code/02_code_models/02A5_GAMM.R
@@ -16,49 +16,51 @@
 
 # Load packages ------------------------------------------------------------
 
-# Detect cores
-options(mc.cores = parallel::detectCores())
-
-# Load libraries
-library(data.table)
-library(brms)
-library(parallel)
+ # Detect cores
+ options(mc.cores = parallel::detectCores())
+ 
+ # Load libraries
+ library(data.table)
+ library(brms)
+ library(parallel)
 
 
 
 # Load data ----------------------------------------------------------------
 
-# Folder path Compute Canada
-folder <- file.path("/home", "maxime11", "projects", "def-monti", 
-                    "maxime11", "phd_project", "data")
-
-# Load data
-data <- fread(file.path(folder, "FraserFrancoetalXXXX-data.csv"),
-              select = c("predator_id",
-                         "latency_1st_capture",
-                         "game_duration",
-                         "cumul_xp_pred"))
-
-# Project path for testing
-#data <- fread("./data/FraserFrancoetalXXXX-data.csv",
-#              select = c("predator_id",
-#                         "latency_1st_capture",
-#                         "game_duration",
-#                         "cumul_xp_pred"))
-
-# Predator id as factor
-data[, predator_id := as.factor(predator_id)]
-
+ # Folder path Compute Canada
+ folder <- file.path("/home", "maxime11", "projects", "def-monti", 
+                     "maxime11", "phd_project", "data")
+ 
+ # Load data
+ data <- fread(file.path(folder, "FraserFrancoetalXXXX-data.csv"),
+               select = c("predator_id",
+                          "latency_1st_capture",
+                          "game_duration",
+                          "cumul_xp_pred"))
+ 
+ # Project path for testing
+ #data <- fread("./data/FraserFrancoetalXXXX-data.csv",
+ #              select = c("predator_id",
+ #                         "latency_1st_capture",
+ #                         "game_duration",
+ #                         "cumul_xp_pred"))
+ 
+ # Predator id as factor
+ data[, predator_id := as.factor(predator_id)]
+ 
+ # Round latency and code as integer
+ data[, latency_1st_capture := as.integer(round(latency_1st_capture))]
 
 
 # Standardise the variables (Z-scores) -------------------------------------
 
-standardize <- function (x) {(x - mean(x, na.rm = TRUE)) / 
-                              sd(x, na.rm = TRUE)}
-
-data[, c("Zgame_duration",
-         "Zcumul_xp") := lapply(.SD, standardize), 
-       .SDcols = c(3:4)]
+ standardize <- function (x) {(x - mean(x, na.rm = TRUE)) / 
+                               sd(x, na.rm = TRUE)}
+ 
+ data[, c("Zgame_duration",
+          "Zcumul_xp") := lapply(.SD, standardize), 
+        .SDcols = c(3:4)]
 
 # ==========================================================================
 # ==========================================================================
@@ -73,33 +75,33 @@ data[, c("Zgame_duration",
 
 # Model formula ------------------------------------------------------------
 
-model_formula <- brmsformula(
-    latency_1st_capture ~
-        s(Zcumul_xp) +
-        s(Zcumul_xp, predator_id, bs = "fs") + 
-        Zgame_duration
-)
+ model_formula <- brmsformula(
+     latency_1st_capture ~
+         s(Zcumul_xp) +
+         s(Zcumul_xp, predator_id, bs = "fs") + 
+         Zgame_duration
+ )
 
 
 
 # Define priors ------------------------------------------------------------
 
-priors <- c(
-  # priors on fixed effects
-  set_prior("normal(0, 1)",
-            class = "b",
-            coef = "Zgame_duration"),
-  set_prior("normal(0, 2)",
-            class = "b",
-            coef = "sZcumul_xp_1"),
-  # prior on the intercept
-  set_prior("normal(0, 0.5)",
-            class = "Intercept")
-  # priors on smooth terms
-  #set_prior("normal(0, 1)",
-  #          class = "sds",
-  #          coef = "s(Zcumul_xp)")
-)
+ priors <- c(
+   # priors on fixed effects
+   set_prior("normal(5, 1)",
+             class = "b",
+             coef = "Zgame_duration"),
+   set_prior("normal(0, 2)",
+             class = "b",
+             coef = "sZcumul_xp_1"),
+   # prior on the intercept
+   set_prior("normal(30, 0.5)",
+             class = "Intercept")
+   # priors on smooth terms
+   #set_prior("normal(0, 1)",
+   #          class = "sds",
+   #          coef = "s(Zcumul_xp)")
+ )
 
 # ==========================================================================
 # ==========================================================================
@@ -112,22 +114,22 @@ priors <- c(
 # 3. Run the model
 # ==========================================================================
 
-model_gs <- brm(formula = model_formula,
-                family = gaussian(),
-                warmup = 500, 
-                iter = 1500,
-                thin = 4,
-                chains = 4,
-                threads = threading(12),
-                backend = "cmdstanr",
-                inits = "0", 
-                seed = 123,
-                prior = priors,
-                sample_prior = TRUE,
-                control = list(adapt_delta = 0.99),
-                data = data)
-
-saveRDS(model_gs, file = "02A5_GAMM.rds")
+ model_gs <- brm(formula = model_formula,
+                 family = poisson(),
+                 warmup = 500, 
+                 iter = 1500,
+                 thin = 4,
+                 chains = 4,
+                 threads = threading(12),
+                 backend = "cmdstanr",
+                 inits = "0", 
+                 seed = 123,
+                 prior = priors,
+                 sample_prior = TRUE,
+                 control = list(adapt_delta = 0.99),
+                 data = data)
+ 
+ saveRDS(model_gs, file = "02A5_GAMM.rds")
 
 # ==========================================================================
 # ==========================================================================
