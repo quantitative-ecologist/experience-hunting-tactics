231d5c8fa900a33fd06536dcf352581b8ec57b28
quantitative-ecologist <49170767+quantitative-ecologist@users.noreply.github.com>
2022-12-19 09:06:02 -0500

    update model draws

--- code/03_code_model-validation/03_A_models-draws.R
+++ code/03_code_model-validation/03_A_models-draws.R
@@ -16,23 +16,25 @@
 
 # Load libraries -------------------------------------------------------------------
 
-library(brms)
-library(ggplot2)
+ library(brms)
+ library(ggplot2)
 
 
 
 # Prepare model draws --------------------------------------------------------------
 
-# Import model in R session
-mod1 <- readRDS("./outputs/02_outputs_models/02A1_GAMM.rds")
-mod2 <- readRDS("./outputs/02_outputs_models/02A2_GAMM.rds")
-mod3 <- readRDS("./outputs/02_outputs_models/02A3_GAMM.rds")
-
-
-# Extract posterior draws
-posterior_fit1 <- as_draws_df(mod1)
-posterior_fit2 <- as_draws_df(mod2)
-posterior_fit3 <- as_draws_df(mod3)
+ path <- file.path(getwd(), "outputs", "02_outputs_models")
+ 
+ # Import model in R session
+ mod1 <- readRDS(file.path(path, "02A1_GAMM.rds"))
+ mod4 <- readRDS(file.path(path, "02A4_GAMM.rds"))
+ mod7 <- readRDS(file.path(path, "02A7_GAMM.rds"))
+ 
+ 
+ # Extract posterior draws
+ posterior_fit1 <- as_draws_df(mod1)
+ posterior_fit2 <- as_draws_df(mod4)
+ posterior_fit3 <- as_draws_df(mod7)
 
 # ==================================================================================
 # ==================================================================================
@@ -49,102 +51,79 @@ posterior_fit3 <- as_draws_df(mod3)
 
 # Plot prior and posterior draws ---------------------------------------------------
 
-# Intercept
-ggplot(posterior_fit1) +
-  geom_density(aes(prior_Intercept),
-               fill = "steelblue",
-               color = "black",
-               alpha = 0.6) +
-  geom_density(aes(Intercept),
-               fill = "#FC4E07",
-               color = "black",
-               alpha = 0.6) + 
-  theme_classic()
-
-
-# Game duration
-ggplot(posterior_fit1) +
-  geom_density(aes(prior_b_Zgame_duration),
-               fill = "steelblue",
-               color = "black",
-               alpha = 0.6) +
-  geom_density(aes(b_Zgame_duration),
-               fill = "#FC4E07",
-               color = "black",
-               alpha = 0.6) + 
-  theme_classic()
-
-
-# slope term of cumul xp
-ggplot(posterior_fit1) +
-  geom_density(aes(prior_bs_sZcumul_xp_1),
-               fill = "steelblue",
-               color = "black",
-               alpha = 0.6) +
-  geom_density(aes(bs_sZcumul_xp_1),
-               fill = "#FC4E07",
-               color = "black",
-               alpha = 0.6) + 
-  theme_classic()
-
-
-# Standard deviation of cumul XP smooth term
-ggplot(posterior_fit1) +
-  geom_density(aes(prior_sds_sZcumul_xp_1),
-               fill = "steelblue",
-               color = "black",
-               alpha = 0.6) +
-  #geom_density(aes(sds_sZcumul_xp_1),
-  #             fill = "#FC4E07",
-  #             color = "black",
-  #             alpha = 0.6) + 
-  theme_classic()
-
-ggplot(posterior_fit1) +
-  #geom_density(aes(prior_sds_sZcumul_xp_1),
-  #             fill = "steelblue",
-  #             color = "black",
-  #             alpha = 0.6) +
-  geom_density(aes(sds_sZcumul_xp_1),
-               fill = "#FC4E07",
-               color = "black",
-               alpha = 0.6) + 
-  theme_classic()  
-
-ggplot(posterior_fit1) +
-  geom_density(aes(prior_sds_sZcumul_xp_1),
-               fill = "steelblue",
-               color = "black",
-               alpha = 0.6) +
-  geom_density(aes(sds_sZcumul_xp_1),
-               fill = "#FC4E07",
-               color = "black",
-               alpha = 0.6) + 
-  theme_classic()  
-
-# Standard deviation of predator ID
-ggplot(posterior_fit1) +
-  geom_density(aes(prior_sds_spredator_id_1),
-               fill = "steelblue",
-               color = "black",
-               alpha = 0.6) +
-  geom_density(aes(sds_spredator_id_1),
-               fill = "#FC4E07",
-               color = "black",
-               alpha = 0.6) + 
-  theme_classic()
-
-# Phi parameter
-ggplot(posterior_fit1) +
-  geom_density(aes(prior_phi),
-               fill = "steelblue",
-               color = "black",
-               alpha = 0.6) +
-  geom_density(aes(phi),
-               fill = "#FC4E07",
-               color = "black",
-               alpha = 0.6) + 
-  theme_classic()
+ # Intercept
+ ggplot(posterior_fit1) +
+   geom_density(aes(prior_Intercept),
+                fill = "steelblue",
+                color = "black",
+                alpha = 0.6) +
+   geom_density(aes(Intercept),
+                fill = "#FC4E07",
+                color = "black",
+                alpha = 0.6) + 
+   theme_classic()
+ 
+ 
+ # Game duration
+ ggplot(posterior_fit1) +
+   geom_density(aes(prior_b_Zgame_duration),
+                fill = "steelblue",
+                color = "black",
+                alpha = 0.6) +
+   geom_density(aes(b_Zgame_duration),
+                fill = "#FC4E07",
+                color = "black",
+                alpha = 0.6) + 
+   theme_classic()
+ 
+ # slope term of cumul xp
+ ggplot(posterior_fit1) +
+   geom_density(aes(prior_bs_sZcumul_xp_1),
+                fill = "steelblue",
+                color = "black",
+                alpha = 0.6) +
+   geom_density(aes(bs_sZcumul_xp_1),
+                fill = "#FC4E07",
+                color = "black",
+                alpha = 0.6) + 
+   theme_classic()
+ 
+ 
+ # Standard deviation of cumul XP smooth term
+ ggplot(posterior_fit1) +
+   geom_density(aes(prior_sds_sZcumul_xp_1),
+                fill = "steelblue",
+                color = "black",
+                alpha = 0.6) +
+   geom_density(aes(sds_sZcumul_xp_1),
+                fill = "#FC4E07",
+                color = "black",
+                alpha = 0.6) + 
+   theme_classic()
+ 
+ # Standard deviation of predator ID
+ ggplot(posterior_fit1) +
+   geom_density(aes(prior_sds_spredator_id_1),
+                fill = "steelblue",
+                color = "black",
+                alpha = 0.6) +
+   geom_density(aes(sds_spredator_id_1),
+                fill = "#FC4E07",
+                color = "black",
+                alpha = 0.6) + 
+   theme_classic()
+ 
+ # Phi parameter
+ ggplot(posterior_fit1) +
+   geom_density(aes(prior_phi),
+                fill = "steelblue",
+                color = "black",
+                alpha = 0.6) +
+   geom_density(aes(phi),
+                fill = "#FC4E07",
+                color = "black",
+                alpha = 0.6) + 
+   theme_classic()
 
 # ==================================================================================
 # ==================================================================================
@@ -154,116 +133,76 @@ ggplot(posterior_fit1) +
 
 
 # ==================================================================================
-# 2. Model 2
+# 2. Model 4
 # ==================================================================================
 
 
 
 # Plot prior and posterior draws ---------------------------------------------------
 
-# Intercept
-ggplot(posterior_fit2) +
-  geom_density(aes(prior_Intercept),
-               fill = "steelblue",
-               color = "black",
-               alpha = 0.6) +
-  geom_density(aes(Intercept),
-               fill = "#FC4E07",
-               color = "black",
-               alpha = 0.6) + 
-  theme_classic()
-
-
-# Game duration
-ggplot(posterior_fit2) +
-  geom_density(aes(prior_b_Zgame_duration),
-               fill = "steelblue",
-               color = "black",
-               alpha = 0.6) +
-  geom_density(aes(b_Zgame_duration),
-               fill = "#FC4E07",
-               color = "black",
-               alpha = 0.6) + 
-  theme_classic()
-
-
-# slope term of cumul xp
-ggplot(posterior_fit2) +
-  geom_density(aes(prior_bs_sZcumul_xp_1),
-               fill = "steelblue",
-               color = "black",
-               alpha = 0.6) +
-  geom_density(aes(bs_sZcumul_xp_1),
-               fill = "#FC4E07",
-               color = "black",
-               alpha = 0.6) + 
-  theme_classic()
-
-
-# Standard deviation of cumul XP smooth term
-ggplot(posterior_fit2) +
-  geom_density(aes(prior_sds_sZcumul_xp_1),
-               fill = "steelblue",
-               color = "black",
-               alpha = 0.6) +
-  geom_density(aes(sds_sZcumul_xp_1),
-               fill = "#FC4E07",
-               color = "black",
-               alpha = 0.6) + 
-  theme_classic()
-
-
-# not sure what this is but refers to predator ID
-ggplot(posterior_fit2) +
-  geom_density(aes(prior_sds_sZcumul_xppredator_id_1),
-               fill = "steelblue",
-               color = "black",
-               alpha = 0.6) +
-  geom_density(aes(sds_sZcumul_xppredator_id_1),
-               fill = "#FC4E07",
-               color = "black",
-               alpha = 0.6) + 
-  theme_classic()
-
-
-# not sure what this is but refers to predator ID
-ggplot(posterior_fit2) +
-  geom_density(aes(prior_sds_sZcumul_xppredator_id_2),
-               fill = "steelblue",
-               color = "black",
-               alpha = 0.6) +
-  geom_density(aes(sds_sZcumul_xppredator_id_2),
-               fill = "#FC4E07",
-               color = "black",
-               alpha = 0.6) + 
-  theme_classic()
-
-
-# not sure what this is but refers to predator ID
-ggplot(posterior_fit2) +
-geom_density(aes(prior_sds_sZcumul_xppredator_id_3),
-             fill = "steelblue",
-             color = "black",
-             alpha = 0.6) +
-geom_density(aes(sds_sZcumul_xppredator_id_3),
-             fill = "#FC4E07",
-             color = "black",
-             alpha = 0.6) + 
-theme_classic()
-
-
-# Phi parameter
-ggplot(posterior_fit2) +
-  geom_density(aes(prior_phi),
-               fill = "steelblue",
-               color = "black",
-               alpha = 0.6) +
-  geom_density(aes(phi),
-               fill = "#FC4E07",
-               color = "black",
-               alpha = 0.6) + 
-  theme_classic()
-
+ # Intercept
+ ggplot(posterior_fit2) +
+   geom_density(aes(prior_Intercept),
+                fill = "steelblue",
+                color = "black",
+                alpha = 0.6) +
+   geom_density(aes(Intercept),
+                fill = "#FC4E07",
+                color = "black",
+                alpha = 0.6) + 
+   theme_classic()
+ 
+ 
+ # Game duration
+ ggplot(posterior_fit2) +
+   geom_density(aes(prior_b_Zgame_duration),
+                fill = "steelblue",
+                color = "black",
+                alpha = 0.6) +
+   geom_density(aes(b_Zgame_duration),
+                fill = "#FC4E07",
+                color = "black",
+                alpha = 0.6) + 
+   theme_classic()
+ 
+ 
+ # slope term of cumul xp
+ ggplot(posterior_fit2) +
+   geom_density(aes(prior_bs_sZcumul_xp_1),
+                fill = "steelblue",
+                color = "black",
+                alpha = 0.6) +
+   geom_density(aes(bs_sZcumul_xp_1),
+                fill = "#FC4E07",
+                color = "black",
+                alpha = 0.6) + 
+   theme_classic()
+ 
+ 
+ # Standard deviation of cumul XP smooth term
+ ggplot(posterior_fit2) +
+   geom_density(aes(prior_sds_sZcumul_xp_1),
+                fill = "steelblue",
+                color = "black",
+                alpha = 0.6) +
+   geom_density(aes(sds_sZcumul_xp_1),
+                fill = "#FC4E07",
+                color = "black",
+                alpha = 0.6) + 
+   theme_classic()
+ 
+ # Standard deviation of predator ID
+ ggplot(posterior_fit2) +
+   geom_density(aes(prior_sds_spredator_id_1),
+                fill = "steelblue",
+                color = "black",
+                alpha = 0.6) +
+   geom_density(aes(sds_spredator_id_1),
+                fill = "#FC4E07",
+                color = "black",
+                alpha = 0.6) + 
+   theme_classic()
+ 
 # ==================================================================================
 # ==================================================================================
 
@@ -272,88 +211,86 @@ ggplot(posterior_fit2) +
 
 
 # ==================================================================================
-# 3. Model 3
+# 3. Model 7
 # ==================================================================================
 
 
 
 # Plot prior and posterior draws ---------------------------------------------------
 
-# Intercept
-ggplot(posterior_fit3) +
-  geom_density(aes(prior_Intercept),
-               fill = "steelblue",
-               color = "black",
-               alpha = 0.6) +
-  geom_density(aes(Intercept),
-               fill = "#FC4E07",
-               color = "black",
-               alpha = 0.6) + 
-  theme_classic()
+ # Intercept
+ ggplot(posterior_fit3) +
+   geom_density(aes(prior_Intercept),
+                fill = "steelblue",
+                color = "black",
+                alpha = 0.6) +
+   geom_density(aes(Intercept),
+                fill = "#FC4E07",
+                color = "black",
+                alpha = 0.6) + 
+   theme_classic()
+ 
+ 
+ # Game duration
+ ggplot(posterior_fit3) +
+   geom_density(aes(prior_b_Zgame_duration),
+                fill = "steelblue",
+                color = "black",
+                alpha = 0.6) +
+   geom_density(aes(b_Zgame_duration),
+                fill = "#FC4E07",
+                color = "black",
+                alpha = 0.6) + 
+   theme_classic()
+ 
+ # slope term of cumul xp
+ ggplot(posterior_fit3) +
+   geom_density(aes(prior_bs_sZcumul_xp_1),
+                fill = "steelblue",
+                color = "black",
+                alpha = 0.6) +
+   geom_density(aes(bs_sZcumul_xp_1),
+                fill = "#FC4E07",
+                color = "black",
+                alpha = 0.6) + 
+   theme_classic()
+ 
+ 
+ # Standard deviation of cumul XP smooth term
+ ggplot(posterior_fit3) +
+   geom_density(aes(prior_sds_sZcumul_xp_1),
+                fill = "steelblue",
+                color = "black",
+                alpha = 0.6) +
+   geom_density(aes(sds_sZcumul_xp_1),
+                fill = "#FC4E07",
+                color = "black",
+                alpha = 0.6) + 
+   theme_classic()
+ 
+ # Standard deviation of predator ID
+ ggplot(posterior_fit3) +
+   geom_density(aes(prior_sds_spredator_id_1),
+                fill = "steelblue",
+                color = "black",
+                alpha = 0.6) +
+   geom_density(aes(sds_spredator_id_1),
+                fill = "#FC4E07",
+                color = "black",
+                alpha = 0.6) + 
+   theme_classic()
 
-
-# Game duration
-ggplot(posterior_fit3) +
-  geom_density(aes(prior_b_Zgame_duration),
-               fill = "steelblue",
-               color = "black",
-               alpha = 0.6) +
-  geom_density(aes(b_Zgame_duration),
-               fill = "#FC4E07",
-               color = "black",
-               alpha = 0.6) + 
-  theme_classic()
-
-# not sure what this is but refers to predator ID
-ggplot(posterior_fit3) +
-  geom_density(aes(prior_sds_sZcumul_xppredator_id_1),
-               fill = "steelblue",
-               color = "black",
-               alpha = 0.6) +
-  geom_density(aes(sds_sZcumul_xppredator_id_1),
-               fill = "#FC4E07",
-               color = "black",
-               alpha = 0.6) + 
-  theme_classic()
-
-
-# not sure what this is but refers to predator ID
-ggplot(posterior_fit3) +
-  geom_density(aes(prior_sds_sZcumul_xppredator_id_2),
-               fill = "steelblue",
-               color = "black",
-               alpha = 0.6) +
-  geom_density(aes(sds_sZcumul_xppredator_id_2),
-               fill = "#FC4E07",
-               color = "black",
-               alpha = 0.6) + 
-  theme_classic()
-
-
-# not sure what this is but refers to predator ID
-ggplot(posterior_fit3) +
-  geom_density(aes(prior_sds_sZcumul_xppredator_id_3),
-               fill = "steelblue",
-               color = "black",
-               alpha = 0.6) +
-  geom_density(aes(sds_sZcumul_xppredator_id_3),
-               fill = "#FC4E07",
-               color = "black",
-               alpha = 0.6) + 
-  theme_classic()
-
-
-# Phi parameter
-ggplot(posterior_fit3) +
-  geom_density(aes(prior_phi),
-               fill = "steelblue",
-               color = "black",
-               alpha = 0.6) +
-  geom_density(aes(phi),
-               fill = "#FC4E07",
-               color = "black",
-               alpha = 0.6) + 
-  theme_classic()
+ # Phi parameter
+ ggplot(posterior_fit3) +
+   geom_density(aes(prior_phi),
+                fill = "steelblue",
+                color = "black",
+                alpha = 0.6) +
+   geom_density(aes(phi),
+                fill = "#FC4E07",
+                color = "black",
+                alpha = 0.6) + 
+   theme_classic()
 
 # ==================================================================================
 # ==================================================================================
