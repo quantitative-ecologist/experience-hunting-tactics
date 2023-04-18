80881fd594d5dae56d95989e90b8e5d0d5c56cf3
Maxime <49170767+quantitative-ecologist@users.noreply.github.com>
2023-04-18 15:19:17 -0400

    updated code in figure 1 to test transparency

--- code/05_code_figures/05_figure1.R
+++ code/05_code_figures/05_figure1.R
@@ -19,14 +19,14 @@
  library(ggplot2)
  library(ggpubr)
  library(viridis)
- 
+
  path <- file.path(getwd(), "outputs", "02_outputs_models")
 
  mod1 <- readRDS(file.path(path, "02A3_GAMM.rds"))
  mod2 <- readRDS(file.path(path, "02A3_prey-GAMM.rds"))
 
 
- 
+
 # Load the data ------------------------------------------------------------
 
  data <- fread("./data/FraserFrancoetal2023-data.csv",
@@ -37,7 +37,7 @@
                           "cumul_xp_pred",
                           "total_xp_pred",
                           "hunting_success"))
- 
+
  data[, predator_id := as.factor(predator_id)]
 
 
@@ -45,7 +45,7 @@
 # Post processing preparations for custom family ---------------------------
 
  expose_functions(mod1, vectorize = TRUE)
- 
+
  # Define the log likelihood function
  log_lik_beta_binomial2 <- function(i, prep) {
    mu <- brms::get_dpar(prep, "mu", i = i)
@@ -54,7 +54,7 @@
    y <- prep$data$Y[i]
    beta_binomial2_lpmf(y, mu, phi, trials)
  }
- 
+
  # Define function for posterior_predict
  posterior_predict_beta_binomial2 <- function(i, prep, ...) {
    mu <- brms::get_dpar(prep, "mu", i = i)
@@ -62,7 +62,7 @@
    trials <- prep$data$vint1[i]
    beta_binomial2_rng(mu, phi, trials)
  }
- 
+
  # Define function for posterior_epred
  posterior_epred_beta_binomial2 <- function(prep) {
    mu <- brms::get_dpar(prep, "mu")
@@ -74,19 +74,19 @@
 
 
 # Setup a custom theme for the plot ----------------------------------------
- 
+
  custom_theme <- theme(
     # axis values size
-    axis.text = element_text(face = "plain", 
+    axis.text = element_text(face = "plain",
                              size = 14,
                              color = "black"),
     # axis ticks lenght
     axis.ticks.length = unit(.15, "cm"),
     # axis ticks width
-    axis.ticks = element_line(size = 0.90, 
+    axis.ticks = element_line(size = 0.90,
                               color = "black"),
     # axis titles size
-    axis.title = element_text(size = 16, 
+    axis.title = element_text(size = 16,
                               face = "plain",
                               color = "black"),
     axis.line = element_line(size = 0.95,
@@ -115,42 +115,42 @@
  # With conditional_smooths, the predictions are on the link scale
  fig1 <- conditional_smooths(mod1, method = "fitted", robust = FALSE)
  fig2 <- conditional_smooths(mod2, method = "fitted", robust = FALSE)
- 
+
  # Extract values in a table
  tab1 <- fig1[[1]]
  tab2 <- fig2[[1]]
- 
+
  # Transform as data.table
   tab1 <- data.table(tab1)
   tab2 <- data.table(tab2)
-  
+
 
  # Back transform x-axis values
  sequence <- (seq(0, 500, 100) - mean(data$cumul_xp_pred))
  standev <- sd(data$cumul_xp_pred)
  scaled_breaks <- sequence / standev
 
- 
- 
+
+
 # Cut fitted values based on player XP ------------------------------------
 
  # Extract player IDs with their total XP from the original data
- xp <- unique(data[,.(predator_id, total_xp_pred)])
- 
+ xp <- unique(data[, .(predator_id, total_xp_pred)])
+
  # Compute non standardized cumulative XP
- tab1[, cumul_xp := (Zcumul_xp*standev)+mean(data$cumul_xp_pred)]
- tab2[, cumul_xp := (Zcumul_xp*standev)+mean(data$cumul_xp_pred)]
- 
+ tab1[, cumul_xp := (Zcumul_xp * standev) + mean(data$cumul_xp_pred)]
+ tab2[, cumul_xp := (Zcumul_xp * standev) + mean(data$cumul_xp_pred)]
+
  # Now merge the two tables adding the total XP
  tab1 <- merge(tab1, xp, by = "predator_id")
  tab2 <- merge(tab2, xp, by = "predator_id")
- 
+
  # Cut all matches where fitted values are above total XP
- tab1 <- tab1[cumul_xp <= total_xp_pred,]
- tab2 <- tab2[cumul_xp <= total_xp_pred,]
- 
- 
- 
+ tab1 <- tab1[cumul_xp <= total_xp_pred, ]
+ tab2 <- tab2[cumul_xp <= total_xp_pred, ]
+
+
+
 # Produce the plot --------------------------------------------------------
 
  gamm_plot1 <- ggplot(tab1,
@@ -166,7 +166,19 @@
                        labels = seq(0, 500, 100)) +
     xlab("\nCumulative experience") +
     custom_theme
- 
+
+ #gamm_plot1 <- ggplot(tab1,
+ #                     aes(x = Zcumul_xp,
+ #                         y = plogis(estimate__))) +
+ #   geom_line(aes(alpĥa = predator_id), size = 1) +
+ #   ylab("Hunting success\n") +
+ #   scale_y_continuous(breaks = seq(0, 1, 0.25),
+ #                      limits = c(0, 1)) +
+ #   scale_x_continuous(breaks = scaled_breaks,
+ #                      labels = seq(0, 500, 100)) +
+ #   xlab("\nCumulative experience") +
+ #   custom_theme
+
  gamm_plot2 <- ggplot(tab2,
                       aes(x = Zcumul_xp,
                           y = plogis(estimate__),
@@ -184,20 +196,20 @@
 
 
 # Plot with transparent background ----------------------------------------
- 
+
  # With transparent background and white lines
  custom_theme <- theme(
     # axis values size
-    axis.text = element_text(face = "plain", 
+    axis.text = element_text(face = "plain",
                              size = 14,
                              color = "white"),
     # axis ticks lenght
     axis.ticks.length = unit(.15, "cm"),
     # axis ticks width
-    axis.ticks = element_line(size = 0.90, 
+    axis.ticks = element_line(size = 0.90,
                               color = "white"),
     # axis titles size
-    axis.title = element_text(size = 16, 
+    axis.title = element_text(size = 16,
                               face = "plain",
                               color = "white"),
     axis.line = element_line(size = 0.95,
@@ -205,9 +217,9 @@
     legend.position = "none",
     panel.grid = element_blank(),
     panel.background = element_rect(fill = "transparent"),
-    plot.background = element_rect(fill='transparent', color = NA)
+    plot.background = element_rect(fill = "transparent", color = NA)
  )
- 
+
  gamm_plot1T <- ggplot(tab1,
                       aes(x = Zcumul_xp,
                           y = plogis(estimate__),
@@ -251,7 +263,7 @@
 
 # Prepare figure ------------------------------------------------------------
 
- 
+
  # Arrange paneled figure
  figure <- ggarrange(
     NULL, gamm_plot1, NULL, gamm_plot2,
@@ -259,11 +271,11 @@
     labels = c("(A)", "", "(B)", ""),
     widths = c(0.15, 1.5, 0.15, 1.5)
  )
- 
+
 # Export the figure -----------------------------------------------------
 
  path <- file.path(getwd(), "outputs", "05_outputs_figures")
- 
+
  ggexport(figure,
           filename = file.path(path, "05_figure1.png"),
           width = 3000,
@@ -273,7 +285,7 @@
 
  # Transparent figure
  path <- file.path(getwd(), "tests")
- 
+
  # Arrange paneled figure
  figureT <- ggarrange(
    NULL, gamm_plot1T, NULL, gamm_plot2T,
