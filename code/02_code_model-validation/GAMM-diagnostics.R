8f620f8a40aea05f994c11418cda05dbbad449ca
Maxime <49170767+quantitative-ecologist@users.noreply.github.com>
2023-05-10 15:43:20 -0400

    update gamm diagnostics and send to calcul can

--- code/02_code_model-validation/GAMM-diagnostics.R
+++ code/02_code_model-validation/GAMM-diagnostics.R
@@ -16,15 +16,20 @@
 
 # Librairies ------------------------------------------------------------
 
+ # Detect cores
+ options(mc.cores = parallel::detectCores())
+
  library(data.table)
  library(brms)
+ library(parallel)
  library(ggpubr)
 
 
 
 # Import the model ------------------------------------------------------
 
- path <- file.path(getwd(), "outputs", "01_outputs_models")
+ #path <- file.path(getwd(), "outputs", "01_outputs_models")
+ path <- getwd()
 
  # Load models
  mod1 <- readRDS(file.path(path, "A1_GAMM.rds"))
@@ -34,11 +39,11 @@
  mod5 <- readRDS(file.path(path, "A3_GAMM-prey.rds"))
 
  # Check object size
- print(object.size(mod1), units = "MB")
- print(object.size(mod2), units = "MB")
- print(object.size(mod3), units = "MB")
- print(object.size(mod4), units = "MB")
- print(object.size(mod5), units = "MB")
+ #print(object.size(mod1), units = "MB")
+ #print(object.size(mod2), units = "MB")
+ #print(object.size(mod3), units = "MB")
+ #print(object.size(mod4), units = "MB")
+ #print(object.size(mod5), units = "MB")
 
 # =======================================================================
 # =======================================================================
@@ -95,11 +100,11 @@
 
 # Trace plots and parameter distributions -------------------------------
 
- plot(mod1)
- plot(mod2)
- plot(mod3)
- plot(mod4)
- plot(mod5)
+ #plot(mod1)
+ #plot(mod2)
+ #plot(mod3)
+ #plot(mod4)
+ #plot(mod5)
 
 
 
@@ -107,32 +112,71 @@
 
  # Check distributions
  pp1 <- pp_check(mod1)
+ pp1 <- pp1 + ggtitle("GAMM - A1") +
+  theme(legend.position = "bottom")
  pp2 <- pp_check(mod2)
+ pp2 <- pp2 + ggtitle("GAMM - A2") +
+  theme(legend.position = "bottom")
  pp3 <- pp_check(mod3)
+ pp3 <- pp3 + ggtitle("GAMM - A3") +
+  theme(legend.position = "bottom")
  pp4 <- pp_check(mod4)
+ pp4 <- pp4 + ggtitle("GAMM - A2 prey") +
+  theme(legend.position = "bottom")
  pp5 <- pp_check(mod5)
+ pp5 <- pp5 + ggtitle("GAMM - A3 prey") +
+  theme(legend.position = "bottom")
 
  # Effects plot (mean variance plot)
  stat1 <- pp_check(mod1, type = "stat_2d")
+ stat1 <- stat1 + ggtitle("GAMM - A1") +
+  theme(legend.position = "bottom")
  stat2 <- pp_check(mod2, type = "stat_2d")
+ stat2 <- stat2 + ggtitle("GAMM - A2") +
+  theme(legend.position = "bottom")
  stat3 <- pp_check(mod3, type = "stat_2d")
+ stat3 <- stat3 + ggtitle("GAMM - A3") +
+  theme(legend.position = "bottom")
  stat4 <- pp_check(mod4, type = "stat_2d")
+ stat4 <- stat4 + ggtitle("GAMM - A2 prey") +
+  theme(legend.position = "bottom")
  stat5 <- pp_check(mod5, type = "stat_2d")
+ stat5 <- stat5 + ggtitle("GAMM - A3 prey") +
+  theme(legend.position = "bottom")
 
  # Predicted means
  mean1 <- pp_check(mod1, type = "stat", stat = "mean")
+ mean1 <- mean1 + ggtitle("GAMM - A1") +
+  theme(legend.position = "bottom")
  mean2 <- pp_check(mod2, type = "stat", stat = "mean")
+ mean2 <- mean2 + ggtitle("GAMM - A2") +
+  theme(legend.position = "bottom")
  mean3 <- pp_check(mod3, type = "stat", stat = "mean")
+ mean3 <- mean3 + ggtitle("GAMM - A3") +
+  theme(legend.position = "bottom")
  mean4 <- pp_check(mod4, type = "stat", stat = "mean")
+ mean4 <- mean4 + ggtitle("GAMM - A2 prey") +
+  theme(legend.position = "bottom")
  mean5 <- pp_check(mod5, type = "stat", stat = "mean")
-
+ mean5 <- mean5 + ggtitle("GAMM - A3 prey") +
+  theme(legend.position = "bottom")
 
  # Error scatter
  e_scat1 <- pp_check(mod1, type = "error_scatter_avg")
+ e_scat1 <- e_scat1 + ggtitle("GAMM - A1") +
+  theme(legend.position = "bottom")
  e_scat2 <- pp_check(mod2, type = "error_scatter_avg")
+ e_scat2 <- e_scat2 + ggtitle("GAMM - A2") +
+  theme(legend.position = "bottom")
  e_scat3 <- pp_check(mod3, type = "error_scatter_avg")
+ e_scat3 <- e_scat3 + ggtitle("GAMM - A3") +
+  theme(legend.position = "bottom")
  e_scat4 <- pp_check(mod4, type = "error_scatter_avg")
+ e_scat4 <- e_scat4 + ggtitle("GAMM - A2 prey") +
+  theme(legend.position = "bottom")
  e_scat5 <- pp_check(mod5, type = "error_scatter_avg")
+ e_scat5 <- e_scat5 + ggtitle("GAMM - A3 prey") +
+  theme(legend.position = "bottom")
 
 
 
@@ -169,7 +213,7 @@
   # Export the figures into a .pdf file
   ggexport(
     plots,
-    filename = file.path(path1, "A1-3_GAMM-diagnostics.pdf")
+    filename = file.path(path, "GAMM-diagnostics.pdf")
   )
 
 
@@ -208,9 +252,6 @@
 
 # Save outputs ----------------------------------------------------------
 
- # Folder path where I will save the outputs
- path1 <- file.path(getwd(), "outputs", "02_outputs_model-validation")
-
  # Save loo
  #saveRDS(loo1, file = file.path(path1, "A1_loo.rds"))
  #saveRDS(loo2, file = file.path(path1, "A2_loo.rds"))
@@ -219,7 +260,7 @@
  #saveRDS(loo5, file = file.path(path1, "A3_loo-prey.rds"))
 
  # Save table
- saveRDS(loo_table, file = file.path(path1, "A1-3_loo-cv-table.rds"))
+ saveRDS(loo_table, file = file.path(path, "GAMM_loo-cv-table.rds"))
 
  # ordre --> 4, 5, 3, 2, 1
 
