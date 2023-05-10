c263c8c4a00aadd4e3ee39b8464a3a383fd2f2c6
Maxime <49170767+quantitative-ecologist@users.noreply.github.com>
2023-05-10 15:49:26 -0400

    update gamm model draws

--- code/02_code_model-validation/GAMM_models-draws.R
+++ code/02_code_model-validation/GAMM_models-draws.R
@@ -1,55 +1,59 @@
-# ==================================================================================
+# =============================================================================
 
 #                       Inspect prior and posterior draws
 #                               from GAMM-GLMM models
 
-# ==================================================================================
+# =============================================================================
 
 
 
 
 
-# ==================================================================================
+# =============================================================================
 # Prepare session
-# ==================================================================================
+# =============================================================================
 
 
-# Load libraries -------------------------------------------------------------------
+# Load libraries --------------------------------------------------------------
 
  library(brms)
  library(ggplot2)
 
 
 
-# Prepare model draws --------------------------------------------------------------
+# Prepare model draws ---------------------------------------------------------
 
  path <- file.path(getwd(), "outputs", "01_outputs_models")
- 
+
  # Import model in R session
  mod1 <- readRDS(file.path(path, "A1_GAMM.rds"))
  mod2 <- readRDS(file.path(path, "A2_GAMM.rds"))
  mod3 <- readRDS(file.path(path, "A3_GAMM.rds"))
- 
- 
+ mod2p <- readRDS(file.path(path, "A2_GAMM-prey.rds"))
+ mod3p <- readRDS(file.path(path, "A3_GAMM-prey.rds"))
+
+
  # Extract posterior draws
  posterior_fit1 <- as_draws_df(mod1)
  posterior_fit2 <- as_draws_df(mod2)
  posterior_fit3 <- as_draws_df(mod3)
+ posterior_fit4 <- as_draws_df(mod2p)
+ posterior_fit5 <- as_draws_df(mod3p)
 
-# ==================================================================================
-# ==================================================================================
+# =============================================================================
+# =============================================================================
 
 
 
 
 
-# ==================================================================================
+# =============================================================================
 # 1. Model 1
-# ==================================================================================
+# =============================================================================
 
 
 
-# Plot prior and posterior draws ---------------------------------------------------
+# Plot prior and posterior draws ----------------------------------------------
 
  # Intercept
  ggplot(posterior_fit1) +
@@ -62,8 +66,8 @@
                 color = "black",
                 alpha = 0.6) + 
    theme_classic()
- 
- 
+
+
  # Game duration
  ggplot(posterior_fit1) +
    geom_density(aes(prior_b_Zgame_duration),
@@ -73,9 +77,9 @@
    geom_density(aes(b_Zgame_duration),
                 fill = "#FC4E07",
                 color = "black",
-                alpha = 0.6) + 
+                alpha = 0.6) +
    theme_classic()
- 
+
  # slope term of cumul xp
  ggplot(posterior_fit1) +
    geom_density(aes(prior_bs_sZcumul_xp_1),
@@ -85,10 +89,10 @@
    geom_density(aes(bs_sZcumul_xp_1),
                 fill = "#FC4E07",
                 color = "black",
-                alpha = 0.6) + 
+                alpha = 0.6) +
    theme_classic()
- 
- 
+
+
  # Standard deviation of cumul XP smooth term
  ggplot(posterior_fit1) +
    geom_density(aes(prior_sds_sZcumul_xp_1),
@@ -98,9 +102,9 @@
    geom_density(aes(sds_sZcumul_xp_1),
                 fill = "#FC4E07",
                 color = "black",
-                alpha = 0.6) + 
+                alpha = 0.6) +
    theme_classic()
- 
+
  # Standard deviation of predator ID
  ggplot(posterior_fit1) +
    geom_density(aes(prior_sds_spredator_id_1),
@@ -110,9 +114,9 @@
    geom_density(aes(sds_spredator_id_1),
                 fill = "#FC4E07",
                 color = "black",
-                alpha = 0.6) + 
+                alpha = 0.6) +
    theme_classic()
- 
+
  # Phi parameter
  ggplot(posterior_fit1) +
    geom_density(aes(prior_phi),
@@ -125,20 +129,20 @@
                 alpha = 0.6) + 
    theme_classic()
 
-# ==================================================================================
-# ==================================================================================
+# =============================================================================
+# =============================================================================
 
 
 
 
 
-# ==================================================================================
-# 2. Model 4
-# ==================================================================================
+# =============================================================================
+# 2. Model 2
+# =============================================================================
 
 
 
-# Plot prior and posterior draws ---------------------------------------------------
+# Plot prior and posterior draws ----------------------------------------------
 
  # Intercept
  ggplot(posterior_fit2) +
@@ -149,10 +153,10 @@
    geom_density(aes(Intercept),
                 fill = "#FC4E07",
                 color = "black",
-                alpha = 0.6) + 
+                alpha = 0.6) +
    theme_classic()
- 
- 
+
+
  # Game duration
  ggplot(posterior_fit2) +
    geom_density(aes(prior_b_Zgame_duration),
@@ -162,10 +166,10 @@
    geom_density(aes(b_Zgame_duration),
                 fill = "#FC4E07",
                 color = "black",
-                alpha = 0.6) + 
+                alpha = 0.6) +
    theme_classic()
- 
- 
+
+
  # slope term of cumul xp
  ggplot(posterior_fit2) +
    geom_density(aes(prior_bs_sZcumul_xp_1),
@@ -175,10 +179,10 @@
    geom_density(aes(bs_sZcumul_xp_1),
                 fill = "#FC4E07",
                 color = "black",
-                alpha = 0.6) + 
+                alpha = 0.6) +
    theme_classic()
- 
- 
+
+
  # Standard deviation of cumul XP smooth term
  ggplot(posterior_fit2) +
    geom_density(aes(prior_sds_sZcumul_xp_1),
@@ -188,35 +192,23 @@
    geom_density(aes(sds_sZcumul_xp_1),
                 fill = "#FC4E07",
                 color = "black",
-                alpha = 0.6) + 
-   theme_classic()
- 
- # Standard deviation of predator ID
- ggplot(posterior_fit2) +
-   geom_density(aes(prior_sds_spredator_id_1),
-                fill = "steelblue",
-                color = "black",
                 alpha = 0.6) +
-   geom_density(aes(sds_spredator_id_1),
-                fill = "#FC4E07",
-                color = "black",
-                alpha = 0.6) + 
    theme_classic()
- 
-# ==================================================================================
-# ==================================================================================
+
+# =============================================================================
+# =============================================================================
 
 
 
 
 
-# ==================================================================================
-# 3. Model 7
-# ==================================================================================
+# =============================================================================
+# 3. Model 3
+# =============================================================================
 
 
 
-# Plot prior and posterior draws ---------------------------------------------------
+# Plot prior and posterior draws ----------------------------------------------
 
  # Intercept
  ggplot(posterior_fit3) +
@@ -227,10 +219,10 @@
    geom_density(aes(Intercept),
                 fill = "#FC4E07",
                 color = "black",
-                alpha = 0.6) + 
+                alpha = 0.6) +
    theme_classic()
- 
- 
+
+
  # Game duration
  ggplot(posterior_fit3) +
    geom_density(aes(prior_b_Zgame_duration),
@@ -240,44 +232,19 @@
    geom_density(aes(b_Zgame_duration),
                 fill = "#FC4E07",
                 color = "black",
-                alpha = 0.6) + 
-   theme_classic()
- 
- # slope term of cumul xp
- ggplot(posterior_fit3) +
-   geom_density(aes(prior_bs_sZcumul_xp_1),
-                fill = "steelblue",
-                color = "black",
                 alpha = 0.6) +
-   geom_density(aes(bs_sZcumul_xp_1),
-                fill = "#FC4E07",
-                color = "black",
-                alpha = 0.6) + 
    theme_classic()
- 
- 
- # Standard deviation of cumul XP smooth term
- ggplot(posterior_fit3) +
-   geom_density(aes(prior_sds_sZcumul_xp_1),
-                fill = "steelblue",
-                color = "black",
-                alpha = 0.6) +
-   geom_density(aes(sds_sZcumul_xp_1),
-                fill = "#FC4E07",
-                color = "black",
-                alpha = 0.6) + 
-   theme_classic()
- 
+
  # Standard deviation of predator ID
  ggplot(posterior_fit3) +
-   geom_density(aes(prior_sds_spredator_id_1),
+   geom_density(aes(prior_sds_sZcumul_xppredator_id_1),
                 fill = "steelblue",
                 color = "black",
                 alpha = 0.6) +
-   geom_density(aes(sds_spredator_id_1),
+   geom_density(aes(sds_sZcumul_xppredator_id_1),
                 fill = "#FC4E07",
                 color = "black",
-                alpha = 0.6) + 
+                alpha = 0.6) +
    theme_classic()
 
  # Phi parameter
@@ -289,8 +256,8 @@
    geom_density(aes(phi),
                 fill = "#FC4E07",
                 color = "black",
-                alpha = 0.6) + 
+                alpha = 0.6) +
    theme_classic()
 
-# ==================================================================================
-# ==================================================================================
+# =============================================================================
+# =============================================================================
