6f4018abc95733895388b372b6da46f7d4e7539e
quantitative-ecologist <49170767+quantitative-ecologist@users.noreply.github.com>
2024-09-07 16:26:52 -0400

    update retrieve_osf_file.r

--- data/retrieve_osf_file.r
+++ data/retrieve_osf_file.r
@@ -1,22 +1,31 @@
 # =======================================================================
 
-# Retrieve GAMM file from OSF repository : https://osf.io/hdv38/
-# Model output from : https://github.com/quantitative-ecologist/predator-expertise
+# Run only once
+
+# This script retrieves a GAMM file from this OSF repository :
+#  https://osf.io/hdv38/
+
+# Original project repository :
+#  https://github.com/quantitative-ecologist/predator-expertise
 
 # =======================================================================
+
 
 # Load osfr
 library(osfr)
 
 # Create dir
-dir.create("osf_folder", showWarnings = FALSE)
+dir.create(file.path(getwd(), "data", "osf_folder"), showWarnings = FALSE)
 
 # List all files in the OSF repository
 files <- osf_retrieve_node("hdv38") %>% osf_ls_files()
 
 # Identify the file ID
-id <- files[files$name=="A3_GAMM-speed-rank.rds",]$id
+id <- files[files$name == "GAMM-V.rds", ]$id
 
-# retrieve id and download the 'osf_folder' folder
+# Specify path to save the file
+path <- file.path(getwd(), "data", "osf_folder")
+
+# Retrieve id and download to 'osf_folder'
 osf_retrieve_file(id) %>%
-  osf_download(path = "osf_folder", conflicts = "overwrite")
+  osf_download(path = path, conflicts = "overwrite")
